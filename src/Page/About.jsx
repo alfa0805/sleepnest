4921bf886cc67df687c88e5ba20a5d49a1eb540a
alfa0805 <asadbekarslonbekov53@gmail.com>
2025-04-14 10:59:32 +0500

    div

--- src/Page/About.jsx
+++ src/Page/About.jsx
@@ -10,6 +10,7 @@ function About() {
     }));
   return (
     <div>
+      <div className=""></div>
       <div class="container">
         <div class="flex flex-col-reverse lg:flex-row md:items-center gap-10">
           <div class="">
